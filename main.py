6a31be27b55aa9c8e2f0fb4923a711d383311318
Дем <firsov.2008@mail.ru>
2023-11-20 16:51:02 +0300

    UI.ui стал UI.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 from random import randint
 
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
+from UI import Ui_MainWindow
 
-class Suprematism(QMainWindow):
+
+class Suprematism(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.qp = QPainter()
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
@@ -27,12 +28,13 @@ class Suprematism(QMainWindow):
         self.update()
 
     def make_ball(self):
-        self.bx = randint(0, self.size().width() - 300 if self.size().width() >= 300 else 0)
-        self.by = randint(0, self.size().height() - 300 if self.size().height() >= 300 else 0)
+        self.bc = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
         self.bs = randint(0, min(self.size().width(), self.size().height()))
+        self.bx = randint(0, self.size().width() - self.bs if self.size().width() >= self.bs else 0)
+        self.by = randint(0, self.size().height() - self.bs if self.size().height() >= self.bs else 0)
 
     def draw(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(self.bc)
         qp.drawEllipse(self.bx * (self.bx > 0), self.by * (self.by > 0), self.bs, self.bs)
 
 
